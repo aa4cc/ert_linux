49197323c4b0ffba82d916aff9e5e7ea15f34830
Pavel Pisa <pisa@cmp.felk.cvut.cz>
2021-01-29 00:46:09 +0100

    Specify that even NuttX target ert_nuttx.tlc supports tcpip external mode.
    Signed-off-by: Pavel Pisa <pisa@cmp.felk.cvut.cz>

--- ert_linux/sl_customization.m
+++ ert_linux/sl_customization.m
@@ -1,3 +1,4 @@
 function sl_customization(cm)
     cm.ExtModeTransports.add('ert_linux.tlc', 'tcpip', 'ext_comm', 'Level1');
+    cm.ExtModeTransports.add('ert_nuttx.tlc', 'tcpip', 'ext_comm', 'Level1');
 end
